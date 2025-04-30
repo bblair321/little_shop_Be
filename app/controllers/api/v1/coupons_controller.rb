4a3f9dccd0481b2e07d40ea765a70ec6fdfdab69
Brady Blair <bblair321@yahoo.comm>
2025-04-30 08:34:42 -0700

    forgot my create method

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -35,6 +35,21 @@ module Api
         }
       end
 
+      def create
+        merchant = Merchant.find(params[:merchant_id])
+        coupon = merchant.coupons.new(coupon_params)
+      
+        if coupon.save
+          render json: {
+            data: format_coupon(coupon)
+          }, status: :created
+        else
+          Rails.logger.error("Coupon creation failed: #{coupon.errors.full_messages}")
+      
+          render json: { error: coupon.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
       def activate
         if @coupon.update(active: true)
           render json: {
@@ -80,6 +95,10 @@ module Api
           type: "coupon",
           attributes: coupon_attributes
         }
+      end
+
+      def coupon_params
+        params.require(:coupon).permit(:name, :code, :discount_type, :discount_value, :active)
       end
     end
   end
